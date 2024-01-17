e930308f1922c8d61d8a68b859141beabe3cb5ef
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2024-01-17 18:19:38 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,10 @@
 $(function () {
     const VISUAL_SLIDE = new Swiper('.MainVisual .visual_slide', {
         loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
         on: {
             slideChange: function () {
                 const index_currentSlide = this.realIndex;
@@ -12,6 +16,28 @@ $(function () {
                     $('.visual_slide_vid').trigger('pause')
                 }
             },
-        }
+
+            slideChangeTransitionEnd: function () {
+                $('.MainVisual .slide_nav .text_box').removeClass('on');
+                $('.MainVisual .slide_nav .text_box').eq(this.realIndex).addClass('on');
+            }
+        },
+
+        slideActiveClass: 'on',
     });
+
+    $('.MainVisual .slide_nav .text_box').on('click', function () {
+        const IDX = $(this).index();
+        console.log(IDX);
+        VISUAL_SLIDE.slideTo(IDX, 200);
+    })
+
+    $(window).on('scroll', function () {
+        const SCT = $(window).scrollTop();
+        if (SCT > 0) {
+            $('#Header .header_wrap').addClass('on')
+        } else {
+            $('#Header .header_wrap').removeClass('on')
+        }
+    })
 })
