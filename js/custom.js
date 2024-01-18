25495a492c60d3753819a13f6a8b8a6b5ad2e459
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2024-01-18 18:18:21 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -30,12 +30,29 @@ $(function () {
         VISUAL_SLIDE.slideToLoop(IDX);
     })
 
-    $(window).on('scroll', function () {
+    $('.MainVisual .slide_nav .nav_arrows .left').on('click', function () {
+        VISUAL_SLIDE.slidePrev();
+    })
+    $('.MainVisual .slide_nav .nav_arrows .right').on('click', function () {
+        VISUAL_SLIDE.slideNext();
+    })
+
+    $('.m_open').on('click', function () {
+        $('#Header').toggleClass('on');
+    })
+
+    $(window).on('scroll', function (e) {
         const SCT = $(window).scrollTop();
         if (SCT > 0) {
             $('#Header .header_wrap').addClass('on')
         } else {
             $('#Header .header_wrap').removeClass('on')
+        }
+    })
+
+    $('#Header').on('wheel touchmove', function (e) {
+        if ($('#Header').hasClass('on')) {
+            e.preventDefault();
         }
     })
 })
